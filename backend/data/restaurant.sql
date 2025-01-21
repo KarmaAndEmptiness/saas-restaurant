7ef9d1f9d836cb4b5a2e1c84dce2d5304bcf0826
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2025-01-21 21:56:21 +0800

    2025.1.21 update

--- backend/data/restaurant.sql
+++ backend/data/restaurant.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `restaurant`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `restaurant` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `restaurant`;
+
+--
 -- Table structure for table `account_record`
 --
 
@@ -546,4 +554,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-21 21:29:39
+-- Dump completed on 2025-01-21 21:55:49
