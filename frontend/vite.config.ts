ad9e8bccc25d9cc9ea37a07cd2fceba5caf17645
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2025-02-17 10:41:26 +0000

    2025.2.17 update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   server: {
     headers: {
       'Service-Worker-Allowed': '/'
-    }
+    },
+  },
+  preview: {
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'holaworld.ggff.net', 'haloworld.ggff.net'],
   },
   resolve: {
     alias: {
