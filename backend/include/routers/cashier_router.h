62fe5b63dd97b505ad5ef2c36b09ec2cdeba8a30
KarmaAndEmptiness <wupeiling@tutamail.com>
2025-05-24 16:59:13 +0000

    重新构建后端

--- backend/include/routers/cashier_router.h
+++
@@ -1,8 +0,0 @@
-#ifndef CASHIER_ROUTER_H
-#define CASHIER_ROUTER_H
-#include "httplib.h"
-namespace router
-{
-    void cashier_router(httplib::Server &server);
-}
-#endif
--- backend/include/routers/marketing_router.h
+++
@@ -1,8 +0,0 @@
-#ifndef MARKETING_ROUTER_H
-#define MARKETING_ROUTER_H
-#include "httplib.h"
-namespace router
-{
-    void marketing_router(httplib::Server &server);
-}
-#endif
