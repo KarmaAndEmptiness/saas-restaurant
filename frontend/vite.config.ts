ac21e642a8047e52197cceb1d7785efbdead51d5
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2025-05-25 00:53:00 +0000

    重新构建前端

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    headers: {
-      'Service-Worker-Allowed': '/'
-    },
-    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'homeless.run.place'],
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  base: '/',
-  preview: {
-    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'homeless.run.place'],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  }
-}) 
+  plugins: [react()],
+})
