132a3e06929f9a58609d8ac4a4422188dfe89732
KarmaAndEmptiness <wupeiling@tutamail.com>
2025-05-25 00:53:00 +0000

    重新构建前端

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
-import { router } from './routes';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-// 开发环境启用 Mock Service Worker
-// if (process.env.NODE_ENV === 'development') {
-//   const { worker } = await import('./mocks/browser');
-//   worker.start();
-// }
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ConfigProvider
-      locale={zhCN}
-      theme={{
-        token: {
-          colorPrimary: '#1890ff',
-        },
-      }}
-    >
-      <RouterProvider router={router} />
-    </ConfigProvider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
