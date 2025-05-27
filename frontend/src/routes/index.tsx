a100f362d4b9e6499099504c5c6ccdd690ab2bea
KarmaAndEmptiness <wupeiling@tutamail.com>
2025-05-27 08:48:18 +0000

    前端暂时提交

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, type DOMRouterOpts, type RouteObject } from "react-router";
 import Login from "@/layouts/Login";
 import Home from "@/layouts/Home";
+import Tenant from '@/views/Tenant'
 import AuthGuard from "@/components/AuthGuard";
+import Log from "@/views/Tenant/Log";
 
 const routes: RouteObject[] = [
   {
@@ -10,11 +12,21 @@ const routes: RouteObject[] = [
   },
   {
     path: '/home',
-    element: <AuthGuard><Home /></AuthGuard>
+    element: <AuthGuard><Home /></AuthGuard>,
+    children: [
+      {
+        path: '',
+        element: <AuthGuard><Tenant /></AuthGuard>
+      },
+      {
+        path: 'log',
+        element: <AuthGuard><Log /></AuthGuard>
+      }
+    ]
   }
 ]
 
 const opts: DOMRouterOpts = {
-  basename: '/restaurant'
+  basename: import.meta.env.VITE_API_BASE
 }
 export default createBrowserRouter(routes, opts)
