8ab73a1bccdd64e331243259ffebd7e22a2cca11
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2025-05-29 02:32:13 +0000

    调整前端环境变量

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite';
-const base=process.env.VITE_API_BASE
+//const base=process.env.VITE_API_BASE
 // https://vite.dev/config/
 export default defineConfig({
-  base,
+  base:'/restaurant',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
